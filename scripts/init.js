ad9db9e95a6b81882b036cf2a0ffafef102f65d5
Jovin James <Jovin_James@swissre.com>
2019-07-31 20:09:34 +0530

    layout changes

--- scripts/init.js
+++ scripts/init.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.profile-header').backstretch([
+      {width: 1080,url:"assets/imgs/header1.jpg",}
+      ,{width: 1080,url:"assets/imgs/header2.jpg",}
+    ], {duration: 1000, fade: 750});
   });
